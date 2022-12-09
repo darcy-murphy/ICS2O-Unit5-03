88f6a95a60b2d2b6ef8ff03dcccbd804a22b7052
darcy-murphy <113056025+darcy-murphy@users.noreply.github.com>
2022-12-09 18:53:47 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,43 +1,36 @@
 
 
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Darcy Murphy All rights reserved
 //
 // Created by: Darcy Murphy
 // Created on: Nov 2022
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Check servie worker.
+ * Check service worker.
  */
- if (navigator.serviceWorker) {
-  navigator.serviceWorker.register(
-    "/ICS2O-Unit5-01/sw.js",
-    {
-      scope:"/ICS2O-Unit5-01/",
-    }
-  )
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-03/sw.js", {
+    scope: "/ICS2O-Unit5-03/",
+  })
 }
 
 /**
- * This function displays the slider value.
- */
-function myButtonClicked() {
-  document.getElementById("slider-value").innerHTML =
-    "<p>Value is: " + slider.value + "</p>"
-}
-
-const randomNumber = Math.floor(Math.random() * 6) + 1
-
-/** 
  * This function updates the slider value.
  */
 function updateSliderValue(valueFromSlider) {
-  document.getElementById("slider-value").innerHTML = valueFromSlider
-  document.getElementById("answer").innerHTML =
-    "The answer was, " + randomNumber + "!" + " You got it! Good job."
-  
-    // block of code to be executed if conditional is true
-  if (valueFromSlider != randomNumber) 
-    document.getElementById("answer").innerHTML = 
-    "The answer was, " + randomNumber + " !" + " Nice guess, but try again."
+  document.getElementById("slider-value").innerHTML = "Your age: " + valueFromSlider
+
+  if (valueFromSlider > 13) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see up to an R rated movie."
+  } else if (valueFromSlider == 13) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see up to a Pg 13 rated movie."
+  } else if (valueFromSlider < 13) {
+    document.getElementById("movie-rating").innerHTML =
+      "You can see up to a G rated movie."
   }
+}
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var GHPATH = "/ICS2O-Unit5-01"
+var GHPATH = "/ICS2O-Unit5-03"
 var APP_PREFIX = "gppwa_"
 var VERSION = "version_001"
 var URLS = [
